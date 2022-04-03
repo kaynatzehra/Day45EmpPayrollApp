f688cedeacb4e3ab68bf3c215a5a02e89d91f191
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-03 20:58:53 +0530

    DisplayFromLocalStorage

--- js/new_home.js
+++ js/new_home.js
@@ -1,87 +1,41 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 });
 
-//UC4
-//template literal ES6
-const createInnerHtml = () => {
-    const headerHtml = "  <th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-
-    
-    let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
-    for (const empPayrollData of empPayrollList){
-        innerHtml = `${innerHtml}
-            <tr>
-                <td>
-                    <img class="profile" src="${empPayrollData._profilePic}" alt="">
-                </td>
-                <td>${empPayrollData._name}</td>
-                <td>${empPayrollData._gender}</td>
-                <td>
-                    <div>${getDeptHtml(empPayrollData._department)}</div>
-                </td>
-                <td>${empPayrollData._salary}</td>
-                <td>${empPayrollData._startDate}</td>
-                <td>
-                    <img name="${empPayrollData._id}"  onclick="remove(this)"
-                        src="/assets/images/delete-black-18dp.svg"
-                        alt="delete">
-                    <img name="${empPayrollData._id}" onclick="update(this)"
-                        src="/assets/images/create-black-18dp.svg" alt="edit">
-                </td>
-            </tr>
-            `;
-    }
-    document.querySelector('#table-display').innerHTML = innerHtml;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+                JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
 
-
-//UC 5 JSON OBJECT
-const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            _name: 'Krati Gupta',
-            _gender: 'Female',
-            _department: [
-                'Engineer', 
-                'Sales'
-            ],
-            _salary: '600000',
-            _startDate: '20 July 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profilepics/pp1.png'
-        },
-        {
-            _name: 'Yash Gupta',
-            _gender: 'Male',
-            _department: [
-                'Engineer', 
-                'Finance'
-            ],
-            _salary: '700000',
-            _startDate: '01 Oct 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profilepics/pp2.png'
-        },
-        {
-            _name: 'Seema Gupta',
-            _gender: 'Female',
-            _department: [
-                'Engineer', 
-                'Finance'
-            ],
-            _salary: '1000000',
-            _startDate: '01 Oct 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profilepics/pp3.png'
-        }
-        
-    ];
-    return empPayrollListLocal; 
+const createInnerHtml = () => {
+    if (empPayrollList.length == 0) return;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}`;
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>
+                <img class="profile" src="${empPayrollData._profilePic}" alt="">
+            </td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${stringifyDate(empPayrollData._startDate)}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)"
+                    src="/assets/images/delete-black-18dp.svg"
+                    alt="delete">
+                <img name="${empPayrollData._id} onclick="update(this)"
+                    src="/assets/images/create-black-18dp.svg" alt="edit">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
 const getDeptHtml = (deptList) => {
